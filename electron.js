d468127baec9882ba5d85e7bb43af678d23d7acb
[dyad] <andrewmnj@gmail.com>
2025-08-13 16:13:17 -0400

    [dyad] Setting up Electron for desktop app - wrote 2 file(s)

---
+++ electron.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // In development, load from the Vite dev server.
+  // In production, load the built index.html file.
+  const startURL = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, 'dist/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  // Open DevTools automatically in development.
+  if (process.env.ELECTRON_START_URL) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
