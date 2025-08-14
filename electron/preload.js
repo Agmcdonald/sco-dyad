2da7eb5020d2d9cc19b4d0193803d982ff0326a0
[dyad] <andrewmnj@gmail.com>
2025-08-14 14:40:53 -0400

    [dyad] Implemented Phase 1 of Electron conversion with main process, preload script, and React integration - wrote 10 file(s), added electron, electron-builder, concurrently, wait-on, electron-store, better-sqlite3, node-stream-zip, sharp, chokidar package(s)

---
+++ electron/preload.js
@@ -0,0 +1,55 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+  // App info
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  
+  // Navigation
+  onNavigateTo: (callback) => {
+    ipcRenderer.on('navigate-to', (event, path) => callback(path));
+  },
+  
+  // File operations
+  onFilesSelected: (callback) => {
+    ipcRenderer.on('files-selected', (event, filePaths) => callback(filePaths));
+  },
+  
+  onFolderSelected: (callback) => {
+    ipcRenderer.on('folder-selected', (event, folderPath) => callback(folderPath));
+  },
+  
+  // File system operations (to be implemented)
+  readComicFile: (filePath) => ipcRenderer.invoke('read-comic-file', filePath),
+  extractCover: (filePath) => ipcRenderer.invoke('extract-cover', filePath),
+  scanFolder: (folderPath) => ipcRenderer.invoke('scan-folder', folderPath),
+  organizeFile: (filePath, targetPath) => ipcRenderer.invoke('organize-file', filePath, targetPath),
+  
+  // Database operations (to be implemented)
+  initDatabase: () => ipcRenderer.invoke('init-database'),
+  saveComic: (comic) => ipcRenderer.invoke('save-comic', comic),
+  getComics: () => ipcRenderer.invoke('get-comics'),
+  updateComic: (comic) => ipcRenderer.invoke('update-comic', comic),
+  deleteComic: (comicId) => ipcRenderer.invoke('delete-comic', comicId),
+  
+  // Settings
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+  
+  // Dialog
+  showMessageBox: (options) => ipcRenderer.invoke('show-message-box', options),
+  
+  // Platform info
+  platform: process.platform,
+  
+  // Remove listeners
+  removeAllListeners: (channel) => {
+    ipcRenderer.removeAllListeners(channel);
+  }
+});
+
+// Security: Remove Node.js globals in renderer process
+delete window.require;
+delete window.exports;
+delete window.module;
---
+++ src/hooks/useElectron.ts
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+
+// Type definitions for our Electron API
+interface ElectronAPI {
+  getAppVersion: () => Promise<string>;
+  onNavigateTo: (callback: (path: string) => void) => void;
+  onFilesSelected: (callback: (filePaths: string[]) => void) => void;
+  onFolderSelected: (callback: (folderPath: string) => void) => void;
+  readComicFile: (filePath: string) => Promise<any>;
+  extractCover: (filePath: string) => Promise<string>;
+  scanFolder: (folderPath: string) => Promise<string[]>;
+  organizeFile: (filePath: string, targetPath: string) => Promise<boolean>;
+  initDatabase: () => Promise<void>;
+  saveComic: (comic: any) => Promise<any>;
+  getComics: () => Promise<any[]>;
+  updateComic: (comic: any) => Promise<any>;
+  deleteComic: (comicId: string) => Promise<boolean>;
+  getSettings: () => Promise<any>;
+  saveSettings: (settings: any) => Promise<void>;
+  showMessageBox: (options: any) => Promise<any>;
+  platform: string;
+  removeAllListeners: (channel: string) => void;
+}
+
+declare global {
+  interface Window {
+    electronAPI?: ElectronAPI;
+  }
+}
+
+export const useElectron = () => {
+  const [isElectron, setIsElectron] = useState(false);
+  const [electronAPI, setElectronAPI] = useState<ElectronAPI | null>(null);
+
+  useEffect(() => {
+    // Check if we're running in Electron
+    if (window.electronAPI) {
+      setIsElectron(true);
+      setElectronAPI(window.electronAPI);
+    }
+  }, []);
+
+  return {
+    isElectron,
+    electronAPI
+  };
+};
+
+// Hook for handling file selection from menu
+export const useElectronFileHandlers = () => {
+  const { electronAPI } = useElectron();
+
+  useEffect(() => {
+    if (!electronAPI) return;
+
+    const handleFilesSelected = (filePaths: string[]) => {
+      console.log('Files selected:', filePaths);
+      // TODO: Process selected files
+    };
+
+    const handleFolderSelected = (folderPath: string) => {
+      console.log('Folder selected:', folderPath);
+      // TODO: Scan folder for comic files
+    };
+
+    electronAPI.onFilesSelected(handleFilesSelected);
+    electronAPI.onFolderSelected(handleFolderSelected);
+
+    return () => {
+      electronAPI.removeAllListeners('files-selected');
+      electronAPI.removeAllListeners('folder-selected');
+    };
+  }, [electronAPI]);
+};
+
+// Hook for handling navigation from menu
+export const useElectronNavigation = () => {
+  const { electronAPI } = useElectron();
+
+  useEffect(() => {
+    if (!electronAPI) return;
+
+    const handleNavigateTo = (path: string) => {
+      // This will be handled by React Router
+      window.location.hash = path;
+    };
+
+    electronAPI.onNavigateTo(handleNavigateTo);
+
+    return () => {
+      electronAPI.removeAllListeners('navigate-to');
+    };
+  }, [electronAPI]);
+};
