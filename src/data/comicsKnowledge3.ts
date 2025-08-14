08935a3077f24e577f22685fbf7128e014839577
[dyad] <andrewmnj@gmail.com>
2025-08-14 10:57:37 -0400

    [dyad] Added Valiant Comics to the knowledge base - wrote 2 file(s)

---
+++ src/data/comicsKnowledge3.ts
@@ -0,0 +1,215 @@
+import { ComicKnowledge } from "@/lib/knowledgeBase";
+
+export const comicsKnowledgeData3: ComicKnowledge[] = [
+  {
+    "series": "X-O Manowar",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "2012", "year": 2012 },
+      { "volume": "2017", "year": 2017 },
+      { "volume": "2020", "year": 2020 }
+    ]
+  },
+  {
+    "series": "Harbinger",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "2012", "year": 2012 }
+    ]
+  },
+  {
+    "series": "Bloodshot",
+    "publisher": "Valiant Comics",
+    "startYear": 1993,
+    "volumes": [
+      { "volume": "1993", "year": 1993 },
+      { "volume": "2012", "year": 2012 },
+      { "volume": "2019", "year": 2019 }
+    ]
+  },
+  {
+    "series": "Archer & Armstrong",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "2012", "year": 2012 }
+    ]
+  },
+  {
+    "series": "Shadowman",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "1997", "year": 1997 },
+      { "volume": "2012", "year": 2012 },
+      { "volume": "2021", "year": 2021 }
+    ]
+  },
+  {
+    "series": "Rai",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "2014", "year": 2014 },
+      { "volume": "2019", "year": 2019 }
+    ]
+  },
+  {
+    "series": "Ninjak",
+    "publisher": "Valiant Comics",
+    "startYear": 1994,
+    "volumes": [
+      { "volume": "1994", "year": 1994 },
+      { "volume": "2015", "year": 2015 },
+      { "volume": "2021", "year": 2021 }
+    ]
+  },
+  {
+    "series": "Unity",
+    "publisher": "Valiant Comics",
+    "startYear": 2013,
+    "volumes": [
+      { "volume": "2013", "year": 2013 }
+    ]
+  },
+  {
+    "series": "Faith",
+    "publisher": "Valiant Comics",
+    "startYear": 2016,
+    "volumes": [
+      { "volume": "2016", "year": 2016 }
+    ]
+  },
+  {
+    "series": "Eternal Warrior",
+    "publisher": "Valiant Comics",
+    "startYear": 1992,
+    "volumes": [
+      { "volume": "1992", "year": 1992 },
+      { "volume": "2013", "year": 2013 }
+    ]
+  },
+  {
+    "series": "Quantum and Woody",
+    "publisher": "Valiant Comics",
+    "startYear": 1997,
+    "volumes": [
+      { "volume": "1997", "year": 1997 },
+      { "volume": "2013", "year": 2013 },
+      { "volume": "2017", "year": 2017 },
+      { "volume": "2020", "year": 2020 }
+    ]
+  },
+  {
+    "series": "Ivar, Timewalker",
+    "publisher": "Valiant Comics",
+    "startYear": 2015,
+    "volumes": [
+      { "volume": "2015", "year": 2015 }
+    ]
+  },
+  {
+    "series": "The Valiant",
+    "publisher": "Valiant Comics",
+    "startYear": 2014,
+    "volumes": [
+      { "volume": "2014", "year": 2014 }
+    ]
+  },
+  {
+    "series": "Divinity",
+    "publisher": "Valiant Comics",
+    "startYear": 2015,
+    "volumes": [
+      { "volume": "2015", "year": 2015 }
+    ]
+  },
+  {
+    "series": "Imperium",
+    "publisher": "Valiant Comics",
+    "startYear": 2015,
+    "volumes": [
+      { "volume": "2015", "year": 2015 }
+    ]
+  },
+  {
+    "series": "Wrath of the Eternal Warrior",
+    "publisher": "Valiant Comics",
+    "startYear": 2015,
+    "volumes": [
+      { "volume": "2015", "year": 2015 }
+    ]
+  },
+  {
+    "series": "Harbinger Renegade",
+    "publisher": "Valiant Comics",
+    "startYear": 2016,
+    "volumes": [
+      { "volume": "2016", "year": 2016 }
+    ]
+  },
+  {
+    "series": "Secret Weapons",
+    "publisher": "Valiant Comics",
+    "startYear": 2017,
+    "volumes": [
+      { "volume": "2017", "year": 2017 }
+    ]
+  },
+  {
+    "series": "Livewire",
+    "publisher": "Valiant Comics",
+    "startYear": 2018,
+    "volumes": [
+      { "volume": "2018", "year": 2018 }
+    ]
+  },
+  {
+    "series": "The Life and Death of Toyo Harada",
+    "publisher": "Valiant Comics",
+    "startYear": 2019,
+    "volumes": [
+      { "volume": "2019", "year": 2019 }
+    ]
+  },
+  {
+    "series": "Savage",
+    "publisher": "Valiant Comics",
+    "startYear": 2016,
+    "volumes": [
+      { "volume": "2016", "year": 2016 },
+      { "volume": "2021", "year": 2021 }
+    ]
+  },
+  {
+    "series": "Doctor Tomorrow",
+    "publisher": "Valiant Comics",
+    "startYear": 2020,
+    "volumes": [
+      { "volume": "2020", "year": 2020 }
+    ]
+  },
+  {
+    "series": "The Harbinger",
+    "publisher": "Valiant Comics",
+    "startYear": 2021,
+    "volumes": [
+      { "volume": "2021", "year": 2021 }
+    ]
+  },
+  {
+    "series": "Armorclads",
+    "publisher": "Valiant Comics",
+    "startYear": 2022,
+    "volumes": [
+      { "volume": "2022", "year": 2022 }
+    ]
+  }
+];
