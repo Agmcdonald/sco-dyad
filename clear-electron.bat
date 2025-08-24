3bced04d6097f5532641d8637cb6be4fe41fddd5
[dyad] <andrewmnj@gmail.com>
2025-08-24 02:21:34 -0400

    [dyad] Enhanced cleanup script to fix packaging file locks - wrote 1 file(s) + extra files edited outside of Dyad

--- clear-electron.bat
+++ clear-electron.bat
@@ -1,16 +1,21 @@
 @echo off
-echo Clearing locked Electron files...
+echo Clearing all locked files for packaging...
 
-REM Stop any running Node processes
-taskkill /f /im node.exe 2>nul
+REM Stop any running processes
+echo Stopping Super Comic Organizer processes...
+taskkill /f /im "Super Comic Organizer.exe" 2>nul
 taskkill /f /im electron.exe 2>nul
+taskkill /f /im node.exe 2>nul
 
-REM Wait a moment
-timeout /t 2 /nobreak >nul
+REM Wait a moment for processes to fully terminate
+echo Waiting for processes to terminate...
+timeout /t 3 /nobreak >nul
 
-REM Force remove the problematic directory
+REM Force remove problematic directories
+echo Removing locked directories...
 rmdir /s /q "node_modules\.ignored" 2>nul
 rmdir /s /q "node_modules\electron" 2>nul
+rmdir /s /q "release" 2>nul
 
-echo Done! You can now try the rebuild again.
+echo Cleanup complete! You can now run the package command.
 pause
